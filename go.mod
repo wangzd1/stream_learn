0e2ba5377caf380c4750b84a62c71d2acb3f7319
wangzhenda <1543376484@qq.com>
2019-10-22 17:38:10 +0800

    add tarsgo learn

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	github.com/EDDYCJY/go-grpc-example v0.0.0-20181014074047-0f68708edbcb
 	github.com/FFmpeg/FFmpeg v0.6.1 // indirect
+	github.com/TarsCloud/TarsGo v0.0.0-20190926095202-2c6dd1e2f41a
 	github.com/giorgisio/goav v0.1.0 // indirect
 	github.com/golang/protobuf v1.3.2
 	github.com/golang/snappy v0.0.1 // indirect
---
+++ tarsgo/client/client.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"stream_learn/tarsgo/TestApp"
+
+	"github.com/TarsCloud/TarsGo/tars"
+)
+
+//tars.Communicator should only init once and be global
+var comm *tars.Communicator
+
+func main() {
+	comm = tars.NewCommunicator()
+	obj := "TestApp.TestServer.HelloObj@tcp -h 127.0.0.1 -p 20001 -t 60000"
+	app := new(TestApp.Hello)
+	comm.StringToProxy(obj, app)
+	var req string = "Hello Wold"
+	var out string = "Hello Wold"
+	ret, err := app.TestHello(req, &out)
+	if err != nil {
+		fmt.Println("err", err)
+		return
+	}
+	fmt.Println(ret, out)
+}
